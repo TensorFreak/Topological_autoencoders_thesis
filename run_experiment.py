3df8ed5e4384083f8ad30c993290a90d47a0de47
TensorFreak <ivanova.iuiu@phystech.com>
2025-05-14 23:57:17 +0300

    update configs usage

--- run_experiment.py
+++ run_experiment.py
@@ -1,5 +1,4 @@
-import hydra
-from omegaconf import DictConfig, OmegaConf
+import argparse
 import torch
 from torch import optim
 from pathlib import Path
@@ -7,50 +6,56 @@ from tqdm import tqdm
 from data_loader import loaders
 from models.autoencoder import LinearAutoencoder, TopologicalAutoencoder
 
-import json
-
 from train_model import train_model
 from evaluate_metrics import evaluate_metrics
 
-@hydra.main(version_base=None, config_path="configs", config_name="train_config")
-def main(cfg: DictConfig) -> None:
+import json
 
+def main():
+    parser = argparse.ArgumentParser(description='Process a JSON configuration file.')
+    parser.add_argument('config_path', help='Path to the JSON configuration file')
+    args = parser.parse_args()
+    with open(args.config_path, 'r') as f:
+            cfg = json.load(f)
+
+    print(f"Running {cfg['experiment_name']}")
+    
     model = LinearAutoencoder(
-                            #input_dim=mice_data.dimension,
-                            # layers_dims=[500, 350, 100],
-                            # activation='relu'
-                            **cfg.arch_configs
+        input_dim=cfg['arch_config']['input_dim'],
+        layers_dims=cfg['arch_config']['layers_dims'],
+        activation=cfg['arch_config']['activation']
     )
     topo_model = TopologicalAutoencoder(
-                                    model, **cfg.model_configs)
-                                    # complex_max_dim=1,
-                                    # min_persistence=1,
-                                    # top_loss_dims=[0, 1], 
-                                    # dims_weights=[1, 0.3],
-                                    # lam=0.3)
+                    model,
+                    complex_max_dim=cfg['model_config']['complex_max_dim'],
+                    min_persistence=cfg['model_config']['min_persistence'],
+                    top_loss_dims=cfg['model_config']['top_loss_dims'],
+                    dims_weights=cfg['model_config']['dims_weights'],
+                    lam=cfg['model_config']['lam']
+    )
     
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print('Available device:', device)
     
     topo_model = topo_model.to(device)
-    optimizer = optim.Adam(topo_model.parameters(), lr=cfg.training_configs.learning_rate)
 
-    train_loader, val_loader, test_loader = loaders(**cfg.training_configs.data_loader)
-
+    optimizer = optim.Adam(topo_model.parameters(), lr=cfg['training_config']['learning_rate'])
+    train_loader, val_loader, test_loader = loaders(
+        data_path=cfg['training_config']['data_path'],
+        train_size=cfg['training_config']['train_size'],
+        val_size=cfg['training_config']['val_size'],
+        batch_size=cfg['training_config']['batch_size'],
+        data_seed=cfg['training_config']['data_seed']
+    )
+    n_epochs = cfg['training_config']['epochs']
     
-    n_epochs = cfg.training_configs.epochs
     topo_model = train_model(topo_model, optimizer, train_loader, val_loader, test_loader, device, n_epochs=n_epochs)
     metrics_values = evaluate_metrics(topo_model, test_loader, device)
 
-    report_file = 'test_reports/' + cfg.experiment_name + '.txt'
+    report_file = 'test_reports/' + cfg['experiment_name'] + '.txt'
     with open(report_file, 'w') as f:
         f.write("\n".join([metric_name + ' = ' + str(metrics_values[metric_name]) for metric_name in metrics_values.keys()]))
     print('Metrics saved to', report_file)
-    # Training loop would go here
-    # print(f"Starting experiment: {cfg.experiment_name}")
-    # print(f"Model architecture: {cfg.model_name}")
-    # print(f"Training hyperparameters: {cfg.training}")
-    # print(f"Data augmentation: {cfg.data.augmentation}")
 
 if __name__ == "__main__":
     main()
